b612e371e0161e813d59b52b1cdb84c3acc4506d
chenfulin <1233690@qq.com>
2022-07-22 15:41:38 +0800

    新增service层

---
+++ src/main/java/com/example/springboottest/service/TestService.java
@@ -0,0 +1,9 @@
+package com.example.springboottest.service.imp;
+
+/**
+ * 测试接口
+ * @author cfl
+ * @date 2022/7/22 15:39
+ */
+public interface TestService {
+}
---
+++ src/main/java/com/example/springboottest/service/imp/TestServviceImp.java
@@ -0,0 +1,9 @@
+package com.example.springboottest.service.imp;
+
+/**
+ * 测试接口实现类
+ * @author cfl
+ * @date 2022/7/22 15:39
+ */
+public class TestServviceImp {
+}
