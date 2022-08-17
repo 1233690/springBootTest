47f445b4ed58c6a0ae738cd7f40ff8c5518765d6
chenfulin <1233690@qq.com>
2022-08-17 10:31:53 +0800

    删减一些冗余代码 更正部分入参

--- src/main/java/com/cfl/springboottest/util/SwaggerConfig.java
+++ src/main/java/com/cfl/springboottest/util/SwaggerConfig.java
@@ -2,13 +2,10 @@ package com.cfl.springboottest.util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.Profiles;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
